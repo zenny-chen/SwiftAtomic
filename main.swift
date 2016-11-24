5c28f44a73bc90ed46366ca9da432d62055616c9
zenny-chen <zenny_chen@sina.com>
2016-11-24 18:28:45 +0800

    Update main.swift
    added atomic array demo

--- main.swift
+++ main.swift
@@ -392,3 +392,162 @@ atomULongTest()
 atomIntPtrTest()
 atomUIntPtrTest()
 
+print("\n--------\n")
+
+/// 通过泛型定义自己的一个原子数组结构体类型
+public struct MyAtomicArray<Element> {
+
+    /// 内部数组容器对象
+    private var mArray = Array<Element>()
+    
+    /// 用于旋锁的原子对象
+    private var mFlag = AtomicBool(value: true)
+    
+    /// 旋锁上锁
+    private mutating func spinLock() {
+        while !mFlag.exchange(with: false) {
+            swiftCPUPause()
+        }
+    }
+    
+    /// 旋锁解锁
+    private mutating func spinUnlock() {
+        mFlag.store(by: true)
+    }
+    
+    /// 默认的构造方法，空实现
+    public init() {
+
+    }
+    
+    /// 用一个数组对本原子数组进行初始化
+    ///
+    /// 这里要注意的是，初始化过程是非原子的
+    /// - parameters:
+    ///     - withArray: 已构建好的一个数组
+    ///
+    public init(withArray: [Element]) {
+        mArray.append(contentsOf: withArray)
+    }
+    
+    /// 通过数组下标索引来访问指定数组元素，此操作是原子的
+    /// - parameters:
+    ///     - index: 指定数组下标索引
+    /// - returns: 当前指定的数组元素对象
+    public subscript(index: Int) -> Element {
+        
+        /// 获取指定元素对象
+        mutating get {
+            spinLock()
+            let value = mArray[index]
+            spinUnlock()
+            return value
+        }
+        
+        /// 更新指定元素对象。默认参数是newValue
+        mutating set {
+            spinLock()
+            mArray[index] = newValue
+            spinUnlock()
+        }
+    }
+    
+    /// 原子添加一个新的元素
+    /// - parameters:
+    ///     - parameter newElement: 指定的新元素对象
+    ///
+    public mutating func append(_ newElement: Element) {
+        spinLock()
+        mArray.append(newElement)
+        spinUnlock()
+    }
+    
+    /// 原子插入一个新的元素
+    /// - parameters:
+    ///     - newElement: 指定的新元素
+    ///     - index: 在指定的index位置插入新元素对象，而此位置原本的元素对象将会往后挪一个索引位置
+    ///
+    public mutating func insert(_ newElement: Element, at index: Int) {
+        spinLock()
+        mArray.insert(newElement, at: index)
+        spinUnlock()
+    }
+    
+    /// 原子移除指定位置的元素
+    /// - parameters:
+    ///     - index: 指定的位置索引
+    ///
+    public mutating func remove(at index: Int) {
+        spinLock()
+        mArray.remove(at: index)
+        spinUnlock()
+    }
+    
+    /// 原子移除本数组所有元素
+    public mutating func removeAll() {
+        spinLock()
+        mArray.removeAll()
+        spinUnlock()
+    }
+    
+    /// 原子获取当前数组的长度，这个是只读属性
+    public var count: Int {
+        
+        mutating get {
+            spinLock()
+            let length = mArray.count
+            spinUnlock()
+            
+            return length
+        }
+    }
+}
+
+// 创建并初始化一个原子数组
+var arr = MyAtomicArray(withArray: [1, 2, 3, 4, 5])
+//var arr = [1, 2, 3, 4, 5]     // 此普通数组可用于测量非原子数组对象的操作结果
+arr[2] += 10
+print("arr length: \(arr.count), arr[2] = \(arr[2])")
+
+arr.removeAll()
+
+// 用于标识用户线程是否已执行完毕
+var isComplete = false
+
+let beginTime = ProcessInfo.processInfo.systemUptime
+
+// 以下将通过用两个线程给原子数组添加0到1000000的元素来测量其操作的原子性
+DispatchQueue.global(qos: .userInitiated).async() {
+    for i in 0 ..< 500_000 {
+        arr.append(i)
+    }
+    
+    isComplete = true
+}
+
+for i in 500_000 ..< 1_000_000 {
+    arr.append(i)
+}
+
+while !isComplete {
+    swiftCPUPause()
+}
+
+let spentTime = ProcessInfo.processInfo.systemUptime - beginTime
+
+print("Time spent: \(spentTime * 1000.0) ms")
+
+var sum = Int64(0)
+for i in 0 ..< arr.count {
+    sum += arr[i]
+}
+
+print("sum = \(sum)")
+
+// 测试结果正确性
+sum = 0
+for i in 0 ..< 1_000_000 {
+    sum += i
+}
+
+print("test sum = \(sum)")
